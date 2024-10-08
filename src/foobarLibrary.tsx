ff754a09f50fdf5f140c8825489a4367b4a50ec3
bobby <bogoblin@gmail.com>
2024-10-08 21:10:11 +0100

    Color averaging and text contrast

--- src/foobarLibrary.tsx
+++ src/foobarLibrary.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import './foobarLibrary.css';
+import fontColorContrast from "font-color-contrast";
 
 interface BeefwebItem {
     columns: Array<string>
@@ -54,7 +55,23 @@ class Track {
 }
 
 class Album {
+    get bgColor(): [number, number, number] {
+        return this._bgColor || [0,0,0];
+    }
+
+    set bgColor(value: [number, number, number]) {
+        if (this._bgColor === null) {
+            this._bgColor = value;
+            if (fontColorContrast(...value) === '#000000') {
+                this._textColor = [0, 0, 0];
+            } else {
+                this._textColor = [255, 255, 255];
+            }
+        }
+    }
     private _tracks: Array<Track>;
+    private _bgColor: [number, number, number] | null = null;
+    private _textColor: [number, number, number] | null = null;
 
     constructor() {
         this._tracks = [];
@@ -78,6 +95,10 @@ class Album {
 
     tracks() {
         return this._tracks;
+    }
+
+    textColor() {
+        return this._textColor || [255,255,255]; // White as the default because black is the default for the background
     }
 }
 
@@ -162,14 +183,40 @@ export function FoobarLibrary({playlistId}: {playlistId: string}) {
 function FoobarAlbum({album, openAlbumId, toggleOpen, closingAlbumId}: {album: Album, openAlbumId: AlbumId|null, toggleOpen: (albumId: AlbumId|null) => void, closingAlbumId: AlbumId|null}) {
     const albumId = album.albumId();
     return <><li key={album.albumId()} onClick={() => toggleOpen(albumId)}>
-        <img crossOrigin={"anonymous"} src={album.artUrl()}/>
+        <img crossOrigin={"anonymous"} src={album.artUrl()} onLoad={(e) => {
+            if (album.bgColor[0] === 0) {
+                album.bgColor = albumColors(e.currentTarget);
+            }
+        }}/>
         <h2>{album.name()}</h2>
     </li><FoobarAlbumDetails album={album} open={albumId === openAlbumId} closing={albumId === closingAlbumId}/></>
 }
 
+function albumColors(element: HTMLImageElement): [number, number, number] {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return [0,0,0];
+    ctx.drawImage(element, 0, 0, 16, 16);
+    const imageData = ctx.getImageData(0, 0, 16, 16);
+    const total = [0,0,0];
+    for (let y=0; y<imageData.height; y++) {
+        for (let x=0; x<imageData.width; x++) {
+            const index = (y*imageData.width+x)*4;
+            total[0] += imageData.data[index+0];
+            total[1] += imageData.data[index+1];
+            total[2] += imageData.data[index+2];
+        }
+    }
+    const [r,g,b] = total.map(v => Math.floor(v/(imageData.width*imageData.height)));
+    return [r,g,b];
+}
+
 function FoobarAlbumDetails({album, open, closing}: {album: Album, open: boolean, closing: boolean}) {
     return <div className={"album-details " + (open?'open':'') + (closing?'closing':'')}
-                style = {{backgroundImage: `url(${album.artUrl()})`}}
+                style = {{
+                    backgroundColor: `rgb(${album.bgColor.join(',')})`,
+                    color: `rgb(${album.textColor().join(',')})`,
+    }}
     >
         <ul>
         {album.tracks().map(track => <li>{track.trackNumber()}. {track.title()}</li>)}
