99f0ee4bd51430c25c676a891b82e0d3ad45b7f0
bobby <bogoblin@gmail.com>
2024-10-08 16:23:49 +0100

    Group tracks into albums

--- src/foobarLibrary.tsx
+++ src/foobarLibrary.tsx
@@ -1,29 +1,122 @@
 import {useEffect, useState} from "react";
 
-class Library {
-    private readonly libraryJson: unknown;
+interface BeefwebItem {
+    columns: Array<string>
+}
 
-    constructor(libraryJson: unknown) {
-        this.libraryJson = libraryJson;
+type AlbumId = string;
+
+class Track {
+    get index(): number | null {
+        return this._index;
+    }
+    private _index: number | null;
+    private readonly columns: Record<string, string>;
+
+    constructor(item: BeefwebItem, columns: Array<string>) {
+        this._index = null;
+        this.columns = {};
+        for (let i=0; i<columns.length; i++) {
+            this.columns[columns[i]] = item.columns[i];
+        }
+    }
+
+    setIndex(index: number) {
+        this._index = index;
+    }
+
+    albumId(): AlbumId {
+        return `${this.albumArtist()} - ${this.album()}`;
+    }
+
+    album() {
+        return this.columns['%album%'];
+    }
+
+    title() {
+        return this.columns['%title%']
+    }
+
+    albumArtist() {
+        return this.columns['%album artist%'];
+    }
+}
+
+class Album {
+    private tracks: Array<Track>;
+
+    constructor() {
+        this.tracks = [];
+    }
+
+    addTrack(track: Track) {
+        this.tracks.push(track);
+    }
+
+    name() {
+        return this.tracks[0].album();
+    }
+}
+
+class Library {
+    public tracks: Array<Track>;
+    public albums: Map<AlbumId, Album>;
+
+    constructor() {
+        this.tracks = [];
+        this.albums = new Map();
+    }
+
+    addTrack(track: Track) {
+        const index = this.tracks.length;
+        track.setIndex(index);
+        this.tracks.push(track);
+        const album = this.albums.get(track.albumId()) || new Album();
+        album.addTrack(track);
+        this.albums.set(track.albumId(), album);
     }
     
     totalCount(): number {
-        // @ts-expect-error We don't know the type of the response yet ig
-        return this.libraryJson['playlistItems']['totalCount'] as number;
+        return this.tracks.length;
     }
+
+    columns(): Array<string> {
+        return ['%title%', '%artist%', '%album artist%', '%album%'];
+    }
+}
+
+async function fetchLibrary(playlistId: string) {
+    const library = new Library();
+    const columns = library.columns();
+    const params = new URLSearchParams({
+        player: 'false',
+        playlists: 'false',
+        playlistItems: 'true',
+        plref: playlistId,
+        plrange: '0:10000',
+        plcolumns: columns.join(',')
+    });
+    const url = `http://localhost:8880/api/query?${params.toString()}`;
+    const response = await (await fetch(url)).json();
+
+    for (const item of response['playlistItems']['items']) {
+        const track = new Track(item, columns);
+        library.addTrack(track);
+    }
+
+    return library;
 }
 
 export function FoobarLibrary({playlistId}: {playlistId: string}) {
     const [library, setLibrary] = useState<Library | null>(null);
 
     useEffect(() => {
-        fetch(`http://localhost:8880/api/query?player=true&playlists=true&playlistItems=true&plref=${playlistId}&plrange=0%3A100000&plcolumns=%25title%25,%25artist%25,%25album%25`)
-            .then(response => response.json())
-            .then(library => setLibrary(new Library(library)))
+        fetchLibrary(playlistId)
+            .then(library => setLibrary(library))
     }, [playlistId])
 
     if (library) {
-        return <span>Songs: {library.totalCount()}</span>
+        return <span>Songs: {library.totalCount()}, Albums: {library.albums.size}</span>
     } else {
         return <span>Loading...</span>
     }
