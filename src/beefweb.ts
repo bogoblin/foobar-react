21b72f4f00881b1e6c35ff89ac21c5b47d2e33d1
bobby <bogoblin@gmail.com>
2024-10-11 16:31:44 +0100

    Animate the playhead

--- src/beefweb.ts
+++ src/beefweb.ts
@@ -1,7 +1,7 @@
 import {Track} from "./foobarLibrary.tsx";
 import createFetchClient from "openapi-fetch";
 import createClient from "openapi-react-query";
-import {paths} from "./beefwebSchema";
+import {components, paths} from "./beefwebSchema";
 
 const fetchClient = createFetchClient<paths>({
     baseUrl: "http://localhost:8880/api"
@@ -18,40 +18,73 @@ export async function play(track: Track) {
     });
 }
 
-export class UpdateEventSource {
-    get playerState(): BeefwebPlayer | null {
-        return this._playerState;
+export interface PlayerTime {
+    milliseconds: number;
+    seconds: number;
+    display: string;
+}
+
+export function playerTimeFromMilliseconds(milliseconds: number) {
+    const seconds = Math.floor(milliseconds/1000);
+    const display = `${Math.floor(seconds/60)}:${(seconds%60).toLocaleString([], {minimumIntegerDigits: 2})}`;
+    return {milliseconds, seconds, display} as PlayerTime;
+}
+
+export class PlayerState {
+    public playerState: components["schemas"]["PlayerState"];
+    private createdAt: DOMHighResTimeStamp;
+
+    constructor(playerState: components["schemas"]["PlayerState"]) {
+        this.playerState = playerState;
+        this.createdAt = performance.now();
     }
-    private playerStateUpdateHook: ((state: BeefwebPlayer) => void) | undefined;
-    private _playerState: BeefwebPlayer | null = null;
+
+    public position() {
+        if (!this.activeItem) return undefined;
+
+        const playerMilliseconds = (this.activeItem.position || 0)*1000;
+        switch (this.playerState.playbackState) {
+            case "playing": {
+                const millisecondsSince = performance.now() - this.createdAt;
+                const milliseconds = playerMilliseconds + millisecondsSince;
+                return playerTimeFromMilliseconds(milliseconds);
+            }
+            case "paused": {
+                return playerTimeFromMilliseconds(playerMilliseconds);
+            }
+            default: return undefined;
+        }
+    }
+
+    public duration() {
+        if (!this.activeItem) return undefined;
+
+        const milliseconds = 1000*(this.activeItem?.duration || 1);
+        return playerTimeFromMilliseconds(milliseconds);
+    }
+
+    get activeItem() {
+        return this.playerState.activeItem;
+    }
+}
+
+export class UpdateEventSource {
+    private playerStateUpdateHook: ((state: PlayerState) => void) | undefined;
+    public playerState: PlayerState | undefined;
 
     constructor() {
         const es = new EventSource('http://localhost:8880/api/query/updates?player=true&trcolumns=%25artist%25%20-%20%25title%25%2C%25artist%25%20-%20%25album%25%20-%20%25title%25&playlists=true&playlistItems=true&plref=p2&plcolumns=%25artist%25%2C%25album%25%2C%25track%25%2C%25title%25%2C%25length%25&plrange=0%3A100000')
         es.onmessage = (e) => {
             const data = JSON.parse(e.data);
-            if (data['player'] && this.playerStateUpdateHook) {
-                this.playerStateUpdateHook(data['player']);
+            const player = data["player"] as components["schemas"]["PlayerState"];
+            if (player && this.playerStateUpdateHook) {
+                this.playerState = new PlayerState(player);
+                this.playerStateUpdateHook(this.playerState);
             }
         }
     }
 
-    setPlayerStateUpdateHook(hook: (state: BeefwebPlayer) => void) {
+    setPlayerStateUpdateHook(hook: (state: PlayerState) => void) {
         this.playerStateUpdateHook = hook;
     }
-}
-
-interface BeefwebPlayer {
-    activeItem: {
-        duration: number;
-        position: number;
-        index: number;
-        playlistId: string;
-        playlistIndex: number;
-        columns: Array<string>;
-    };
-    info: object;
-    options: Array<object>;
-    playbackMode: number;
-    playbackModes: Array<string>;
-    volume: object;
 }
--- src/playbackControls.tsx
+++ src/playbackControls.tsx
@@ -1,17 +1,51 @@
-import {UpdateEventSource} from "./beefweb.ts";
-import {useState} from "react";
+import {PlayerState, PlayerTime, playerTimeFromMilliseconds, UpdateEventSource} from "./beefweb.ts";
+import {useCallback, useEffect, useRef, useState} from "react";
+import './playbackControls.css';
 
 const stream = new UpdateEventSource();
 
 export function PlaybackControls() {
     const [playerState, setPlayerState] = useState(stream.playerState);
     stream.setPlayerStateUpdateHook(setPlayerState);
-    if (playerState) {
-        return <div>
-            <div>{playerState?.activeItem.columns.join(' ')}</div>
-            <div>{playerState.activeItem.position} / {playerState.activeItem.duration}</div>
-        </div>;
+    return <div>
+        <div>{ playerState?.playerState.playbackState }</div>
+        <div>{playerState?.activeItem?.columns?.join(' ')}</div>
+        <Playhead playerState={playerState}/>
+    </div>;
+}
+
+function Playhead({playerState}: {playerState: PlayerState | undefined}) {
+    const [position, setPosition] = useState<PlayerTime | undefined>(undefined);
+
+    // This stores the handle to requestAnimationFrame so that it can be cancelled:
+    const animationRef = useRef(0);
+
+    const updatePlayhead = useCallback(() => {
+        if (playerState) setPosition(playerState.position());
+        animationRef.current = requestAnimationFrame(updatePlayhead);
+    }, [playerState]);
+
+    useEffect(() => {
+        animationRef.current = requestAnimationFrame(updatePlayhead);
+        return () => cancelAnimationFrame(animationRef.current);
+    }, [playerState, updatePlayhead]);
+
+    const duration = playerState
+        ? playerState.duration() : playerTimeFromMilliseconds(0);
+
+    if (position && duration) {
+        return <form>
+            <input type={"range"} id={"playhead"}
+                   min={0} max={duration.milliseconds}
+                   value={position.milliseconds}
+                   onChange={(e) => {
+                       // todo
+                       // setPosition(parseInt(e.target.value));
+                   }}
+            />
+            <div>{`${position.display} / ${duration.display}`}</div>
+        </form>;
     } else {
-        return <div></div>
+        return "Nothing playing";
     }
 }
